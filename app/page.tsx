4d69764fdaa58d32fd78f949371377b523d2efe3
Sanjib Poon <sanjibpoon123@gmail.com>
2023-11-03 11:44:36 +0545

    Remove tic-tac-toe and Add Card following material design guidelines using TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
-import Board from './board';
 
 export default function Home() {
   return (
-    <main>
-      <Board />
+    <main className="grid place-items-center h-screen">
+      {/* Material Design Card */}
+      <div className="bg-white rounded-lg shadow-lg p-6 w-64 h-32 relative">
+        <button className="bg-slate-500 hover:bg-slate-600 rounded-md px-1 absolute bottom-2 right-2">
+          Save
+        </button>
+      </div>
     </main >
   );
 }
